a738a23d70ee8a893bbf47621b0504e400be16a5
ByeonGi <ghgh2090@gmail.com>
2018-09-20 21:50:54 +0900

    ERROR container class duplication

--- react-project1/src/pages/Home/Explanation.js
+++ react-project1/src/pages/Home/Explanation.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './Explanation.css';
 
 const Explanation = () => {
--- react-project1/src/pages/Home/index.js
+++ react-project1/src/pages/Home/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import Explanation from './Explanation';
 import Opinion from './Opinion';
 import List from './List';
-
 import './index.css';
 
 
 const Home = () =>{
     return (
-        <div className = "container">
+        <div className="container">
             <div className="home-container">
                 <Explanation/>
                 <Opinion/>
